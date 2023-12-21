f83f44f08daf1d838d8841ab73df75f47a935003
veilchrome <keeita768900@gmail.com>
2023-12-21 20:09:56 +0700

    Java OOP Project

--- DoOOPjava/src/Person.java
+++ DoOOPjava/src/Person.java
@@ -3,4 +3,25 @@ public class Person {
 	String name;
 	String address;
 	final String country = "Indonesia";
+	
+	Person(String paramName, String paramAddress) {
+		name = paramName;
+		address = paramAddress;
+		
+	}
+	
+	Person(String paramName) {
+		name = paramName;
+	}
+	
+	Person() {
+		
+	}
+	
+	void sayHello(String paramName) {
+		System.out.println("Hello " + paramName + " , My Name is "+ name);
+	}
+	void sayAddress(String paramName) {
+		System.out.println("Alamat Saya" + paramName + name);
+	}
 }
--- DoOOPjava/src/PersonApp.java
+++ DoOOPjava/src/PersonApp.java
@@ -1,10 +1,24 @@
 
 public class PersonApp {
 	public static void main(String[] args ) {
-		var person1 = new Person();
-		Person person2 = new Person();
+		var person1 = new Person("Eko", "Depok");
+		// person1.country = "Amerika" // EROR
+		
+		
+		System.out.println(person1.name);
+		System.out.println(person1.address);
+		System.out.println(person1.country);
+		
+		person1.sayHello("Budi");
+		
+		Person person2 = new Person("Budi");
 		Person person3;
 		person3 = new Person();
+		person3.name = "Selamet";
+		person3.sayHello("Budi");
+		
+		// sesi 2//
+		
 		
 		System.out.println(person1);
 		System.out.println(person2);
